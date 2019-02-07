1cc83e4607561f53a82bb52afb214989c1dbe9e2
Alumno <ulsa@iMacD105.local>
2019-02-07 12:47:45 -0700

    modals

--- resources/views/mascotas/Index.blade.php
+++ resources/views/mascotas/Index.blade.php
@@ -11,7 +11,19 @@
     <div class=box-body>
 <a href="{{route('mascotas.create')}}">
     <button>Agregar mascota</button>
+    </a>
     <div class="table-responsive">
+    <div class="alert alert-success alert-dismissible" style="margin-top: 20px">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                Success alert preview. This alert is dismissable.
+              </div>
+              <div class="alert alert-danger alert-dismissible" style="margin-top: 20px>
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+                Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my entire
+                soul, like these sweet mornings of spring which I enjoy with my whole heart.
+              </div>
     <table class='table'>
         <thead>
             <tr>
@@ -30,15 +42,43 @@
                     
                     <i class="fa fa-fw fa-edit"></i>
                     </a>
-                    
-                    <form method="POST" 
+                    <button class="btn btn-danger" data-toggle="modal" data-target="#modalBorrar{{$mascota->id}}">
+                        <i class="fa fa-fw fa-trash"></i>
+                    </button>
+                    <div class="modal fade" id="modalBorrar{{$mascota->id}}">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Eliminar mascota</h4>
+              </div>
+              <div class="modal-body">
+                <p>Estas seguro que deseas eliminar a {{$mascota->nombre}}?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancelar</button>
+                
+                <form method="POST" 
+                        action="{{route('mascotas.destroy',$mascota->id)}}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-primary">Eliminar</button>
+                    </form>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+                    <!--<form method="POST" 
                         action="{{route('mascotas.destroy',$mascota->id)}}">
                         @csrf
                         @method('DELETE')
                     <a type="submit" class="btn btn-danger">
                         <i class="fa fa-fw fa-trash"></i>
                     </a>
-                    </form>
+                    </form>-->
                     </td>
                 </tr>
             @endforeach
